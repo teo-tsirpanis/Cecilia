1571afcc8010ad5e0349fa04ded1eecab3cb950e
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-02-21 18:30:04 +0200

    Move some bit field-related code away from Mixin.

---
+++ src/Cecilia/BitFieldExtensions.cs
@@ -0,0 +1,66 @@
+﻿// This file is part of Cecilia.
+// Licensed under the MIT License.
+
+namespace Cecilia
+{
+    internal static class BitFieldExtensions
+    {
+        public static bool GetAttributes(this uint self, uint attributes)
+        {
+            return (self & attributes) != 0;
+        }
+
+        public static uint SetAttributes(this uint self, uint attributes, bool value)
+        {
+            if (value)
+                return self | attributes;
+
+            return self & ~attributes;
+        }
+
+        public static bool GetMaskedAttributes(this uint self, uint mask, uint attributes)
+        {
+            return (self & mask) == attributes;
+        }
+
+        public static uint SetMaskedAttributes(this uint self, uint mask, uint attributes, bool value)
+        {
+            if (value)
+            {
+                self &= ~mask;
+                return self | attributes;
+            }
+
+            return self & ~(mask & attributes);
+        }
+
+        public static bool GetAttributes(this ushort self, ushort attributes)
+        {
+            return (self & attributes) != 0;
+        }
+
+        public static ushort SetAttributes(this ushort self, ushort attributes, bool value)
+        {
+            if (value)
+                return (ushort)(self | attributes);
+
+            return (ushort)(self & ~attributes);
+        }
+
+        public static bool GetMaskedAttributes(this ushort self, ushort mask, uint attributes)
+        {
+            return (self & mask) == attributes;
+        }
+
+        public static ushort SetMaskedAttributes(this ushort self, ushort mask, uint attributes, bool value)
+        {
+            if (value)
+            {
+                self = (ushort)(self & ~mask);
+                return (ushort)(self | attributes);
+            }
+
+            return (ushort)(self & ~(mask & attributes));
+        }
+    }
+}
--- src/Cecilia/IMemberDefinition.cs
+++ src/Cecilia/IMemberDefinition.cs
@@ -10,10 +10,8 @@
 
 namespace Cecilia
 {
-
     public interface IMemberDefinition : ICustomAttributeProvider
     {
-
         string Name { get; set; }
         string FullName { get; }
 
@@ -21,67 +19,5 @@ namespace Cecilia
         bool IsRuntimeSpecialName { get; set; }
 
         TypeDefinition DeclaringType { get; set; }
-    }
-
-    static partial class Mixin
-    {
-
-        public static bool GetAttributes(this uint self, uint attributes)
-        {
-            return (self & attributes) != 0;
-        }
-
-        public static uint SetAttributes(this uint self, uint attributes, bool value)
-        {
-            if (value)
-                return self | attributes;
-
-            return self & ~attributes;
-        }
-
-        public static bool GetMaskedAttributes(this uint self, uint mask, uint attributes)
-        {
-            return (self & mask) == attributes;
-        }
-
-        public static uint SetMaskedAttributes(this uint self, uint mask, uint attributes, bool value)
-        {
-            if (value)
-            {
-                self &= ~mask;
-                return self | attributes;
-            }
-
-            return self & ~(mask & attributes);
-        }
-
-        public static bool GetAttributes(this ushort self, ushort attributes)
-        {
-            return (self & attributes) != 0;
-        }
-
-        public static ushort SetAttributes(this ushort self, ushort attributes, bool value)
-        {
-            if (value)
-                return (ushort)(self | attributes);
-
-            return (ushort)(self & ~attributes);
-        }
-
-        public static bool GetMaskedAttributes(this ushort self, ushort mask, uint attributes)
-        {
-            return (self & mask) == attributes;
-        }
-
-        public static ushort SetMaskedAttributes(this ushort self, ushort mask, uint attributes, bool value)
-        {
-            if (value)
-            {
-                self = (ushort)(self & ~mask);
-                return (ushort)(self | attributes);
-            }
-
-            return (ushort)(self & ~(mask & attributes));
-        }
     }
 }
